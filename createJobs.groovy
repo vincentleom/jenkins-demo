3d1f2b8b4fd4938a67b7051d5721146e422dbcf8
arslanonur <arslanonur_@hotmail.com>
2020-11-15 22:24:40 +0300

    Change jenkins configs

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/vincentleom/spring-boot-api_tkgregory.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
